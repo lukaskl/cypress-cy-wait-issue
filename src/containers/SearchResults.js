711cd0510c85441f30bf553c8edf6796a0665375
lukas <lklusis@otravo.eu>
2018-01-30 15:53:44 +0100

    added search results component

--- src/containers/SearchResults.js
+++ src/containers/SearchResults.js
@@ -1,9 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-const SearchResults = () => (
-  <div>
-    <h1>SearchResults</h1>
-  </div>
-);
+export const client = axios.create({
+  baseURL: 'http://www.example.com',
+  headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+  responseType: 'json',
+});
+
+class SearchResults extends Component {
+
+  async getSearchResults() {
+    try {
+      const searchIdResponse = await client.post('/getSearchId', {
+        testParam: 'testParamValue'
+      });
+
+      this.setState({ searchIdResponse: searchIdResponse.data })
+      const searchId = searchIdResponse.data.searchId;
+
+      const searchResultsResponse = await client.post('/getSearchResults', { searchId });
+      this.setState({ searchResults: searchResultsResponse.data })
+    } catch (err) {
+      this.setState({ err: err.message })
+    }
+  }
+
+  componentWillMount() {
+    this.getSearchResults();
+  }
+
+  render() {
+
+    return (
+      <div>
+        <h1>SearchResults</h1>
+        <div>{JSON.stringify(this.state)}</div>
+      </div>
+    );
+  }
+
+}
 
 export default SearchResults;
